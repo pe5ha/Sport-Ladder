bbcc834bfad662c6141ce29becee47b9d1459235
pe5ha <pavel.naumenko.2000@yandex.ru>
2023-05-27 20:57:54 +0300

    add framework codebase

---
+++ GAS Telegram bot Framework/googleTable/Users.js
@@ -0,0 +1,59 @@
+// версия 1
+
+let user = {
+  telegramID: null,
+  nick: null,
+  name: null,
+  currentAction: null,
+  role: null,
+  rowInTable: null,
+  isNewUser: null,
+  tariff: null,
+};
+
+function makeUser(rowInTable, telegramID,nick,name,currentAction=null,role=null,tariff=null,isNewUser=false){
+  user.telegramID= telegramID;
+  user.nick= nick;
+  user.name= name;
+  user.currentAction= currentAction;
+  user.role= role;
+  user.rowInTable= rowInTable;
+  user.isNewUser= isNewUser;
+  user.tariff= tariff;
+  return user;
+}
+
+
+/**
+ * 
+ * @deprecated - is need fix
+ */
+function getUseridByUsername(username) {
+  let idlist = tUsers.use().getRange('D:D').getValues();
+  // idlist = idlist.flat(); // делает 2д массив одномерным
+  for (let i = 0; i < idlist.length; i++) {
+    for (let j = 0; j < idlist[i].length; j++) {
+      if (idlist[i][j].toLowerCase() === username.toLowerCase()) {
+        let row = i + 1;
+        return tUsers.use().getRange(row, 2).getValue();
+      }
+    }
+  }
+  return null;
+}
+
+
+function setUserRole(user,role){
+  tUsers.use().getRange(user.rowInTable, tUsers.getCol(tUsers.role_Title)+1).setValue(role);
+  user.role=role;
+}
+
+function setUserCurrentAction(user,currentAction){
+  tUsers.use().getRange(user.rowInTable, tUsers.getCol(tUsers.current_action_Title)+1).setValue(currentAction);
+}
+
+
+function setUserLanguage(user,language_code){
+  user.language_code = language_code;
+  tUsers.use().getRange(user.rowInTable, tUsers.getCol(tUsers.language_code)+1).setValue(language_code);
+}
---
+++ GAS Telegram bot Framework/main/DirectMessage.js
@@ -0,0 +1,11 @@
+// версия 1
+
+function directMessage(){
+
+  // initial user checking
+  userRegister(user_id);
+
+  // Buisnes Logic
+  useCases();
+
+}
